5c862b625bf872c189a8134a4e6670beee247aa2
Fabio Rehm <fgrehm@gmail.com>
2013-02-18 11:40:17 -0300

    Update phantomjs to 1.8.1

--- site.pp
+++ site.pp
@@ -151,8 +151,8 @@ file {
 ##################################
 # PhantomJS
 exec { 'install-phantomjs':
-  command => 'curl http://phantomjs.googlecode.com/files/phantomjs-1.7.0-linux-x86_64.tar.bz2 | sudo tar xjfv - &&
-              sudo ln -s /usr/local/share/phantomjs-1.7.0-linux-x86_64/bin/phantomjs /usr/local/bin/phantomjs',
+  command => 'curl http://phantomjs.googlecode.com/files/phantomjs-1.8.1-linux-x86_64.tar.bz2 | sudo tar xjfv - &&
+              sudo ln -s /usr/local/share/phantomjs-1.8.1-linux-x86_64/bin/phantomjs /usr/local/bin/phantomjs',
   cwd     => '/usr/local/share',
   creates => '/usr/local/bin/phantomjs'
 }
