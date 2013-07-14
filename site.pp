753bef6559577ac1b0e35c1508f8eec995de6e42
Fabio Rehm <fgrehm@gmail.com>
2013-07-13 22:10:52 -0300

    Upgrade phantomjs

--- site.pp
+++ site.pp
@@ -125,8 +125,8 @@ exec { 'gem regenerate_binstubs"':
 ##################################
 # PhantomJS
 exec { 'install-phantomjs':
-  command => 'curl http://phantomjs.googlecode.com/files/phantomjs-1.8.1-linux-x86_64.tar.bz2 | sudo tar xjfv - &&
-              sudo ln -s /usr/local/share/phantomjs-1.8.1-linux-x86_64/bin/phantomjs /usr/local/bin/phantomjs',
+  command => 'curl https://phantomjs.googlecode.com/files/phantomjs-1.9.1-linux-x86_64.tar.bz2 | sudo tar xjfv - &&
+              sudo ln -s /usr/local/share/phantomjs-1.9.1-linux-x86_64/bin/phantomjs /usr/local/bin/phantomjs',
   cwd     => '/usr/local/share',
   creates => '/usr/local/bin/phantomjs'
 }
